5bb9b87c7b52db3fbf24e06a2a9a50fa54bec9f4
Lauri Levälehto <lauri.levalehto@protonmail.com>
2021-04-09 13:19:33 +0300

    A couple input of fields added to UI.

--- app/src/main/java/com/example/timezonecalculator/MainActivity.kt
+++ app/src/main/java/com/example/timezonecalculator/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.timezonecalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.timezonecalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Bind to activity_main.xml
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val timeZones = resources.getStringArray(R.array.timeZones)
+
+        val arrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, timeZones)
+        binding.inputTimeZoneAutoCompleteTextView.setAdapter(arrayAdapter)
     }
 }
